5c1034840dc3e19115aa1737d74f894d0d877f48
VisionHealth01 <vishalbabaraoharkal@gmail.com>
2025-07-02 11:20:01 +0530

    Create hi.js

---
+++ src/utils/src/utils/src/screens/src/screens/src/i18n/src/i18n/hi.js
@@ -0,0 +1,11 @@
+export default {
+  welcome: 'हेल्थ फ्रेंड्स में आपका स्वागत है!',
+  journal: 'जर्नल',
+  mood: 'मूड ट्रैकर',
+  reminders: 'रिमाइंडर',
+  chatbot: 'एआई चैटबॉट',
+  voice: 'वॉइस जर्नल',
+  logout: 'लॉगआउट',
+  reminder_set: 'रिमाइंडर सेट ✅',
+  daily_reminder: 'आपको हर सुबह 9:00 बजे रिमाइंडर मिलेगा'
+};
